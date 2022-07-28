737dffca26481acc0211ec453d8e06499a3a56d9
1chooo <hugo970217@gmail.com>
2022-07-28 10:42:20 +0800

    MA1003&PH1032

---
+++ MA1003/Ex06.py
@@ -0,0 +1,8 @@
+a = int( input( " a = " ))
+b = int( input( " b = " ))
+if a != b :
+    
+    print( " different " )
+    
+else :
+    print( " same " )
---
+++ MA1003/Ex07.py
@@ -0,0 +1,5 @@
+for i in range( 10 , 100 ) :
+    
+    if i % 2 == 0 or i % 3 == 0 or i % 5 ==0 : continue
+        
+    print( i , end =" ")
