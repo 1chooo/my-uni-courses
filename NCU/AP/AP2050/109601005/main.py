6f6108b1ad01c4e93ff0b08f38ce8a05e83022bb
1chooo <hugo970217@gmail.com>
2022-09-20 08:42:01 +0800

    Fix: Course

--- NCU/AP/AP2050/109601005/main.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
-import cartopy.feature as cfeat
-import numpy as np
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
